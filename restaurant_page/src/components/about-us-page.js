eb40b2c8c423324927ec1e1c050ea7c1387fcbf2
Steven Leif <eugeniusstevenleif18@gmail.com>
2026-09-06 16:19:32 +0700

    craeting about us page

---
+++ restaurant_page/src/components/about-us-page.js
@@ -0,0 +1,58 @@
+import "../css/about-page-styles.css";
+
+function makeSectionCard(heading, desc){
+    const sectionCard = document.createElement("section");
+    sectionCard.setAttribute("class", "section-card");
+
+    const h1 = document.createElement("h1");
+    h1.textContent = heading;
+
+    const p = document.createElement("p");
+    p.textContent = desc;
+
+    sectionCard.appendChild(h1);
+    sectionCard.appendChild(p);
+
+    return sectionCard
+}
+
+export default function loadAboutUsPage() {
+    const content = document.getElementById("content");
+
+    const container = document.createElement("div");
+    container.setAttribute("class", "container");
+
+    container.appendChild(
+        makeSectionCard(
+            "About us",
+            "Sushi To selalu berkomitmen untuk memberikan cita rasa yang otentik dan pelayanan yang baik"
+        )
+    );
+    container.appendChild(
+        makeSectionCard(
+            "Address",
+            "Sushi To selalu berkomitmen untuk memberikan cita rasa yang otentik dan pelayanan yang baik"
+        )
+    );
+    container.appendChild(
+        makeSectionCard(
+            "Telephone",
+            "0812345678"
+        )
+    );
+    container.appendChild(
+        makeSectionCard(
+            "Email",
+            "sushiTo@sushito.com"
+        )
+    );
+
+    const schedule = makeSectionCard("Opening Hours", "Tuesday – Thursday: 5:00 PM – 10:00 PM");
+    schedule.appendChild(document.createTextNode("Friday – Saturday: 5:00 PM – 11:00 PM"));
+    schedule.appendChild(document.createTextNode("Sunday: 5:00 PM – 9:00 PM"));
+    schedule.appendChild(document.createTextNode("Monday: Closed"));
+
+    container.appendChild(schedule);
+    
+    content.appendChild(container);
+}
--- restaurant_page/src/index.js
+++ restaurant_page/src/index.js
@@ -1,6 +1,8 @@
 import "./styles.css";
 import loadHomePage from "./components/home-page.js";
 import loadMenuPage from "./components/menu-page.js";
+import loadAboutUsPage from "./components/about-us-page.js";
 
 // loadHomePage();
-loadMenuPage();
+// loadMenuPage();
+// loadAboutUsPage();
